3469e4b13a826d94c981b77e1b5ce48be15af285
Hector Cho <yc3135@columbia.edu>
2025-08-10 13:44:36 +0900

    change in riskpage

--- src/pages/RiskPage.tsx
+++ src/pages/RiskPage.tsx
@@ -65,9 +65,10 @@ const ChartSelect: React.FC<ChartSelectProps> = ({ onChartChange }) => {
             PMESII
           </MenuItem>
           <MenuItem
-            value={
-              "https://raw.githubusercontent.com/hectorcho/ktitan-public/refs/heads/main/pmesiid_2025-08-04.csv"
-            }
+            value={JSON.stringify({
+              title: "PMESII Simulated",
+              url: "https://raw.githubusercontent.com/hectorcho/ktitan-public/refs/heads/main/pmesiid_2025-08-04.csv"
+            })}
           >
             PMESII Simulated
           </MenuItem>
